df9370e89b1009658a6cd1a3a0fe0ead610f6543
DangerousTod <engwalljonathanthereal@gmail.com>
2018-02-14 23:33:37 -0800

    javascript chess engine

---
+++ pvtable.js
@@ -0,0 +1,49 @@
+/*
+
+PvTable[10000]
+
+Entry .posKey .Move
+
+index = posKey % 10000;
+
+PvTable[index].move = move from AB posKey = GameBoard.posKey
+
+*/
+function GetPvLine(depth) {
+    var move = ProbePvTable();
+    var count = 0;
+
+    while(move != NOMOVE && count < depth) {
+        if(MoveExists(move) == BOOL.TRUE) {
+            MakeMove(move);
+            GameBoard.PvArray[count++] = move;
+        } else {
+            break;
+        }
+        move = ProbePvTable();
+    }
+    while(GameBoard.ply > 0) {
+        TakeMove();
+    }
+    return count;
+}
+
+function ProbePvTable() {
+    var index = GameBoard.posKey % PVENTRIES;
+
+    if(GameBoard.PvTable[index].posKey == GameBoard.posKey) {
+        return GameBoard.PvTable[index].move;
+    }
+    return NOMOVE;
+}
+
+function StorePvMove(move) {
+    var index = GameBoard.posKey % PVENTRIES;
+    GameBoard.PvTable[index].posKey = GameBoard.posKey;
+    GameBoard.PvTable[index].move = move;
+}
+
+
+
+
+
